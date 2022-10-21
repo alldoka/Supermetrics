312165466d8e1925d514ce2b45a21fc27be719e9
Sergey Kuzminich <KuzminichSergey@yandex.ru>
2022-10-21 22:07:30 +0300

    Report stats. Average count of posts per user calculator

--- module/Statistics/src/Calculator/NoopCalculator.php
+++ module/Statistics/src/Calculator/NoopCalculator.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace Statistics\Calculator;
 
@@ -9,12 +9,29 @@ use Statistics\Dto\StatisticsTo;
 
 class NoopCalculator extends AbstractCalculator
 {
+    /** @var int total amount of all posts during a month */
+    private int $postsCount = 0;
+    /** @var int total amount of users active during a month */
+    private int $usersCount = 0;
+    /** @var array ids of users who posted during a month */
+    private array $userIds = [];
+
+    protected const UNITS = 'posts';
+
     /**
      * @inheritDoc
      */
     protected function doAccumulate(SocialPostTo $postTo): void
     {
-        // Noops!
+        $userId = $postTo->getAuthorId();
+
+        // I could use in_array($userId, $this->userIds, true), but O(1) is better than O(n)
+        if (! isset($this->userIds[$userId])) {
+            $this->userIds[$userId] = 0;
+            $this->usersCount++;
+        }
+
+        $this->postsCount++;
     }
 
     /**
@@ -22,6 +39,7 @@ class NoopCalculator extends AbstractCalculator
      */
     protected function doCalculate(): StatisticsTo
     {
-        return new StatisticsTo();
+        return (new StatisticsTo())->setValue(value: $this->postsCount / $this->usersCount);
     }
+
 }
